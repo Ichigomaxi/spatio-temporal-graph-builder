3f3543ea4e8f6ffef84af11e08d7c7dff129f79e
Maximilian Listl <maximilian.listl@tum.de>
2022-05-30 20:44:14 +0200

    tested tracking

--- utils/refactor_eval_summary.py
+++ utils/refactor_eval_summary.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     
     # change this
     dirpath ="/media/HDD2/students/maximilian/spatio-temporal-gnn/experiments"
-    file_subdir = "05-26_17:50_evaluation/mot_metrics/eval_results"        
+    file_subdir = "05-30__18-40_evaluation/eval_results"        
 
     metrics_summary_path = 'metrics_summary.json'
     metric_details_path = "metrics_details.json"
